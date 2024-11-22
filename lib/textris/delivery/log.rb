e75a52dc3f2ef9ffb07bddcf058a2a50be759ada
Philippe Bernery <philippe@bernery.fr>
2024-11-22 10:36:26 +0100

    🐛 Fix log level configuration
    The log level configuration of Textris was actually bypassing all log levels set in the code, leading to logging everything in production.
    
    This commit fixes the issue by keeping the default behaviour of Rails.logger.

--- lib/textris/delivery/log.rb
+++ lib/textris/delivery/log.rb
@@ -1,8 +1,6 @@
 module Textris
   module Delivery
     class Log < Textris::Delivery::Base
-      AVAILABLE_LOG_LEVELS = %w{debug info warn error fatal unknown}
-
       def deliver(to)
         log :info,  "Sent text to #{Phony.format(to)}"
         log :debug, "Texter: #{message.texter || 'UnknownTexter'}" + "#" +
@@ -21,12 +19,6 @@ module Textris
       private
 
       def log(level, message)
-        level = Rails.application.config.try(:textris_log_level) || level
-
-        unless AVAILABLE_LOG_LEVELS.include?(level.to_s)
-          raise(ArgumentError, "Wrong log level: #{level}")
-        end
-
         Rails.logger.send(level, message)
       end
     end
