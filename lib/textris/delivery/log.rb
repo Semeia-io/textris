41a188950b31ad1304faf1dc32c4327f35a31105
Philippe Bernery <philippe@bernery.fr>
2024-11-22 10:53:32 +0100

    ♻️ Use block style logging

--- lib/textris/delivery/log.rb
+++ lib/textris/delivery/log.rb
@@ -2,24 +2,30 @@ module Textris
   module Delivery
     class Log < Textris::Delivery::Base
       def deliver(to)
-        log :debug, "Sent text to #{Phony.format(to)}"
-        log :info, "Texter: #{message.texter || 'UnknownTexter'}" + "#" +
-          "#{message.action || 'unknown_action'}"
-        log :info, "Date: #{Time.now}"
-        log :info, "From: #{message.from || message.twilio_messaging_service_sid || 'unknown'}"
-        log :debug, "To: #{message.to.map { |i| Phony.format(to) }.join(', ')}"
-        log :debug, "Content: #{message.content}"
+        Rails.logger.debug("Textris") { "Sent text to #{Phony.format(to)}" }
+        Rails
+          .logger
+          .info("Textris") do
+            "Texter: #{message.texter || "UnknownTexter"}" + "#" +
+              "#{message.action || "unknown_action"}"
+          end
+        Rails.logger.info("Textris") { "Date: #{Time.now}" }
+        Rails
+          .logger
+          .info("Textris") do
+            "From: #{message.from || message.twilio_messaging_service_sid || "unknown"}"
+          end
+        Rails
+          .logger
+          .debug("Textris") do
+            "To: #{message.to.map { |i| Phony.format(to) }.join(", ")}"
+          end
+        Rails.logger.debug("Textris") { "Content: #{message.content}" }
         (message.media_urls || []).each_with_index do |media_url, index|
           logged_message = index == 0 ? "Media URLs: " : "            "
           logged_message << media_url
-          log :debug, logged_message
+          Rails.logger.debug("Textris") { logged_message }
         end
-      end
-
-      private
-
-      def log(level, message)
-        Rails.logger.send(level, message)
       end
     end
   end
